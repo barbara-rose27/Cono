9b61115384e013f2b61ccec19b97f2d0187220d2
Barbara Kiyomoto <barbara-k@ws20is065.naist.jp>
2023-01-26 22:27:01 +0900

    commit

--- src/components/ShowCard.vue
+++ src/components/ShowCard.vue
@@ -80,7 +80,8 @@
 
         // 非同期側
         async apiSubmit(formtext) {
-        return await this.axios.post('http://localhost:5000/emotion_checker/?text=' + formtext,)
+            return await this.axios.post('http://cono.pythonanywhere.com/emotion_checker/?text=' + formtext,)
+            //return await this.axios.post('http://localhost:5000/emotion_checker/?text=' + formtext,)
     .then(response => {
       // 成功時の処理etc
       console.log(response.data)
@@ -95,7 +96,8 @@
   sendAPI(formtext){
       
       var result;
-      this.axios.get('http://localhost:5000/emotion_checker/?text=' + formtext, 
+        //this.axios.get('http://localhost:5000/emotion_checker/?text=' + formtext, 
+        this.axios.get('http://cono.pythonanywhere.com/emotion_checker/?text=' + formtext, 
               ).then(response =>{
                 //console.log(response.data)
 
